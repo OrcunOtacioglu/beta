2875d4f39b8bd96c9c0d057dee4b78d799c87d04
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-09-15 10:04:15 +0300

    Created and updated API endpoints

--- modules/API/Http/routes.php
+++ modules/API/Http/routes.php
@@ -1,9 +1,24 @@
 <?php
 
-Route::namespace('Modules\API\Http\Controllers')->prefix('api')->group(function () {
+// @TODO IMPLEMENT API MIDDLEWARE
+Route::namespace('Modules\API\Http\Controllers')->prefix('api')->group(function ()
+{
+    // MANAGEMENT RELATED
     Route::resource('/organizer', 'OrganizerController', ['except' => ['create', 'edit']]);
+    Route::resource('/account', 'AccountController', ['except' => ['create', 'edit']]);
+    Route::resource('/role', 'RoleController', ['except' => ['create', 'edit']]);
+    Route::resource('/permission', 'PermissionController', ['except' => 'create', 'edit']);
+
+    // CONTENT RELATED
     Route::resource('/category', 'CategoryController', ['except' => ['create', 'edit']]);
     Route::resource('/event', 'EventController', ['except' => ['create', 'edit']]);
+    Route::resource('/rate', 'RateController', ['except' => ['create', 'edit']]);
+
+    // EVENT RELATED
     Route::resource('/attendee', 'AttendeeController', ['except' => ['create', 'edit']]);
+
+    // COMMERCE RELATED
     Route::resource('/order', 'OrderController', ['except' => ['create', 'edit']]);
+    Route::resource('/order-items', 'OrderItemController', ['except' => ['create', 'edit']]);
+
 });
